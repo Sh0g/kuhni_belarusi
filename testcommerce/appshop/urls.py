26b6559287dae0b40b3298afe6ea848b403173f5
DESKTOP-OQQ1199\User <agentlis001@gmail.com>
2024-04-09 16:23:30 +0300

    added functions for add, update and delete item

--- testcommerce/appshop/urls.py
+++ testcommerce/appshop/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from appshop.views import index, index_item
+from appshop.views import index, index_item, add_item, update_item, delete_item
 
 app_name="appshop"
 
 urlpatterns = [
+    path("", index, name="index"),
     path("main/", index),
     path("product/<id>/", index_item, name="detail"),
+    path("main/additem/", add_item, name="additem"),
+    path("updateitem/<id>/", update_item, name="update_item"),
+    path("deleteitem/<id>/", delete_item, name="delete_item")
+
 ]
--- testcommerce/appshop/views.py
+++ testcommerce/appshop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 
@@ -15,3 +15,37 @@ def index_item(request, id):
         'item': item
     }
     return render(request, "appshop/detail.html", context=context)
+
+def add_item(request):
+    if request.method=="POST":
+        name = request.POST.get("name")
+        prices = request.POST.get("prices")
+        description = request.POST.get("description")
+        image= request.FILES.get("upload")
+        item = Product(name=name, prices=prices, description=description, image=image)
+        item.save()
+    return render(request, "appshop/additem.html")
+
+def update_item(request, id):
+    item = Product.objects.get(id=id)
+    if request.method=="POST":
+        item.name = request.POST.get("name")
+        item.prices = request.POST.get("prices")
+        item.description = request.POST.get("description")
+        item.image= request.FILES.get("upload", item.image)
+        item.save()
+        redirect("/appshop/")
+    context = {
+        'item': item
+    }
+    return render(request, "appshop/update.html", context=context)
+
+def delete_item(request, id):
+    item = Product.objects.get(id=id)
+    if request.method=="POST":
+        item.delete()
+        redirect("/appshop/")
+    context = {
+        'item': item
+    }
+    return render(request, "appshop/delete.html", context=context)
